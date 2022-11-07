7a9285add6214d7cddd36d9db8506cf35188cd51
agustinsalas0 <103864679+agustinsalas0@users.noreply.github.com>
2022-11-07 17:27:00 -0300

    Ultimo cambio pre entrega
    Agregue documentacion, inserte mas datos para mostrar mejor los query y probé todos los scripts.

--- TP1/insert_database.sql
+++ TP1/insert_database.sql
@@ -1,5 +1,6 @@
+
 /*
- * this script contains query sql, for insert data in database
+ * This script contains query sql, it is used to insert data in the database.
 *
 a. Las 3 sucursales existentes actualmente.
 b. Al menos 3 salas por sucursal.
@@ -8,14 +9,16 @@ d. Al menos 5 peli´culas (una de ellas es Argentina, 1985, y otra de ellas es d
 ficción).
 e. Al menos 5 funciones (algunas de ellas deben ocurrir entre el 24 y 
 el 31 de octubre de
-2022).
+2022)
  * 
  *  */
 use webapp;
+
 /*SUCURSALES */
 INSERT INTO sucursales(Localidad, Cant_Salas)VALUES ('Rosario', 3);
 INSERT INTO sucursales(Localidad, Cant_Salas)VALUES ('Córdoba', 3);
 INSERT INTO sucursales(Localidad, Cant_Salas)VALUES ('La plata', 3);
+
 /*SALAS*/
 INSERT INTO salas(Nro, Cant_Butacas,Id_Sucursal)VALUES (1,20,1);
 INSERT INTO salas(Nro, Cant_Butacas,Id_Sucursal)VALUES (2,20,1);
@@ -28,6 +31,7 @@ INSERT INTO salas(Nro, Cant_Butacas,Id_Sucursal)VALUES (3,20,2);
 INSERT INTO salas(Nro, Cant_Butacas,Id_Sucursal)VALUES (1,20,3);
 INSERT INTO salas(Nro, Cant_Butacas,Id_Sucursal)VALUES (2,20,3);
 INSERT INTO salas(Nro, Cant_Butacas,Id_Sucursal)VALUES (3,20,3);
+
 /*BUTACAS*/
 INSERT INTO butacas(Nro,Id_Salas)VALUES (1,1);
 INSERT INTO butacas(Nro,Id_Salas)VALUES (2,1);
@@ -223,17 +227,33 @@ INSERT INTO butacas(Nro,Id_Salas)VALUES (20,9);
  * ejemplo: 
  * https://es.wikipedia.org/wiki/The_Terminator#:~:text=The%20Terminator%20(conocida%20en%20algunos,Linda%20Hamilton%20y%20Michael%20Biehn.
  * */
+
+/*PELICULAS*/
 INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('ciencia ficción','Volver al futuro',1,1);
 INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Comedia','Esperando la carroza',1,0);
 INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Drama','Argentina, 1985',1,0);
 INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Ciencia ficción','El exterminador',1,1);
+INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Terror','El Conjuro 2',0,1);
 INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Drama','Mente indomable',1,1);
+INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Drama','Licorice Pizza',1,1);
+INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Romance','HER',1,1);
+INSERT INTO peliculas(Genero,Nombre,ATP,Subtitulado) VALUES('Comedia','Son como niños',1,1);
 
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',8,1);
+select * from peliculas
+
+/*FUNCIONES*/
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',8,1);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','17:00:00',8,3);
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',8,1);
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',8,1);
+
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-25','19:00:00',4,9);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-25','18:20:00',2,9);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-26','23:00:00',2,8);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-26','16:00:00',4,8);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-25','18:20:00',2,7);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-26','23:00:00',2,7);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-26','16:00:00',4,5);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-26','16:00:00',4,5);
+
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','19:00:00',4,2);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','19:00:00',2,3);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','23:00:00',2,4);
@@ -242,86 +262,154 @@ insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','1
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',6,1);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',7,1);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',9,1);
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',10,2);
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',11,3);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',6,2);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',2,3);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',4,3);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',5,3);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',6,3);
 insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',7,3);
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',9,3);
-insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',11,3);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (3.10,12345678,1,1);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,1245678,2,2);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (3.10,12345678,3,1);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,1245678,4,2);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (3.10,12345678,5,1);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,1245678,6,2);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,38126423,16,61);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,43523754,16,62);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,23126413,16,63);
-insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,16,72);
-/*
-select * from funciones
-select * from peliculas
-*/
-/*A*/
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',1,3);
+insert into funciones(Fecha,Horario,Id_Sala,Id_Pelicula) values ('2022-10-24','16:00:00',5,3);
+
+
+/*COMPRAS*/
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (3.10,12345678,25,1);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,1245678,25,2);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (3.10,12345678,4,21);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,1245678,4,22);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (3.10,12345678,5,23);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,1245678,5,25);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,38126423,7,24);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,43523754,7,31);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,23126413,12,32);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,13,24);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,43523754,20,25);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,23126413,20,63);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,21,72);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,21,73);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,21,74);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,21,75);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,14,71);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,14,72);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,417354230,9 ,74);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,10,72);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,10,74);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,11,72);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,6,72);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,3,75);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,26,90);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,15,90);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,15,91);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,15,93);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,22,90);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,23,103);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,23,104);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,16,109);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,16,110);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,19,108);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,19,107);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,17,120);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,17,121);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,24,120);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,1,140);
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,2,141);
+
+insert into compras(Precio,DNI,Id_Funcion,Id_Butaca) values (4.10,41735423,18,170);
+
+
+
+/*A) Cuantas funciones hay en la sucursal de La Plata?*/
+
 select count(*) as 'Cantidad de Funciones de sucursal de La Plata' from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 where sucursales.Localidad = 'La plata';
-/*B*/
-select peliculas.Nombre as 'Películas en cartelera, sucursal de Córdoba, 24 de octubre' from funciones
+
+/*B) Cuales son las peliculas en la cartelera en una fecha determinada en la sucursal de Cordoba? */
+select distinct peliculas.Nombre as 'Películas en cartelera, sucursal de Córdoba, 24 de octubre' from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 inner join peliculas on peliculas.Id = funciones.Id_Pelicula
 where sucursales.Localidad = 'Córdoba' AND funciones.Fecha = '2022-10-24';
-/*C Averiguar si con "horarios disponibles" se refieren a filtrar tambien por funciones con butacas libres, por ejemplo, en la realidad no seria una funcion disponible si ya no hay butacas libres*/
-select funciones.Horario as 'Horarios disponibles para ver Argentina, 1985 el dia 24 de octubre en Rosario' from funciones
+
+/*C) Cuales son los horarios disponibles para ver la pelicula Argentina, 1985 en una fecha
+determinada en la sucursal de Rosario?*/
+select funciones.Horario as 'Horarios disponibles para ver Argentina, 1985 el dia 24 de octubre en Rosario', salas.Id AS 'Nro de sala' from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 inner join peliculas on peliculas.Id = funciones.Id_Pelicula
 where sucursales.Localidad = 'Rosario' AND funciones.Fecha = '2022-10-24' AND peliculas.Nombre = 'Argentina, 1985';
-/*D*/
+
+/*D) Cuales son los horarios disponibles para ver la pelicula Argentina, 1985 en una fecha
+determinada para cada sucursal? Muestre estos resultados ordenados cronologicamente
+de forma creciente.*/
 select funciones.Horario as 'Horarios disponibles para ver Argentina, 1985 el dia 24 de octubre', sucursales.Localidad as Sucursal from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 inner join peliculas on peliculas.Id = funciones.Id_Pelicula
 where funciones.Fecha = '2022-10-24' AND peliculas.Nombre = 'Argentina, 1985'
 order by funciones.Horario;
-/*E*/
+
+/*E) Cuales peliculas de ficcion hay en la cartelera la semana del 24 de octubre de 2022 en la sucursal Rosario?*/
 select distinct peliculas.Nombre as 'Peliculas de ciencia ficción en Rosario', funciones.Fecha, funciones.Horario from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 inner join peliculas on peliculas.Id = funciones.Id_Pelicula
 where funciones.Fecha >= '2022-10-24' AND funciones.Fecha <= '2022-10-30' AND peliculas.Genero = 'ciencia ficción';
-/*F*/
-select butacas.Nro as 'Butacas vendidas de la funcion 16, sucursal de Córdoba, pelicula Argentina, 1985'  from funciones
+
+/*F) Cuales son las butacas vendidas para Argentina, 1985 en Cordoba en una funcion determinada?*/
+select butacas.Nro as 'Butacas vendidas de la funcion 21, sucursal de Córdoba, pelicula Argentina, 1985'  from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 inner join peliculas on peliculas.Id = funciones.Id_Pelicula
 inner join butacas on butacas.Id_Salas = salas.Id
 inner join compras on (compras.Id_Funcion= funciones.Id AND compras.Id_Butaca = butacas.Id)
-where sucursales.Localidad='Córdoba' AND funciones.Id = 16 AND peliculas.Nombre= 'Argentina, 1985';
-/*G
+where sucursales.Localidad='Córdoba' AND funciones.Id = 21 AND peliculas.Nombre= 'Argentina, 1985';
+
+/*G) Cuales son las butacas libres para ver Argentina, 1985 en Cordoba en una funcion determinada?
 Lo que hice fue poner el mismo select del 4f dentro del where del 4g, haciendo que muestre las butacas que no estan (not in) en el select del 4f*/
 select butacas.Nro as 'Butacas libres de la funcion 16, sucursal de Córdoba, pelicula Argentina, 1985' from funciones
 inner join salas on salas.Id = funciones.Id_Sala
 inner join sucursales on sucursales.Id = salas.Id_Sucursal
 inner join peliculas on peliculas.Id = funciones.Id_Pelicula
 inner join butacas on butacas.Id_Salas = salas.Id
-where sucursales.Localidad='Córdoba' AND funciones.Id = 16 AND peliculas.Nombre= 'Argentina, 1985' AND butacas.Nro not in (
-  select butacas.Nro as 'Butacas vendidas de la funcion 16, sucursal de Córdoba, pelicula Argentina, 1985'  from funciones
+where sucursales.Localidad='Córdoba' AND funciones.Id = 21 AND peliculas.Nombre= 'Argentina, 1985' AND butacas.Nro not in (
+  select butacas.Nro as 'Butacas vendidas de la funcion 21, sucursal de Córdoba, pelicula Argentina, 1985'  from funciones
   inner join salas on salas.Id = funciones.Id_Sala
   inner join sucursales on sucursales.Id = salas.Id_Sucursal
   inner join peliculas on peliculas.Id = funciones.Id_Pelicula
   inner join butacas on butacas.Id_Salas = salas.Id
   inner join compras on (compras.Id_Funcion= funciones.Id AND compras.Id_Butaca = butacas.Id)
-  where sucursales.Localidad='Córdoba' AND funciones.Id = 16 AND peliculas.Nombre= 'Argentina, 1985'
+  where sucursales.Localidad='Córdoba' AND funciones.Id = 21 AND peliculas.Nombre = 'Argentina, 1985'
 )
-/*H
+
+
+
+/*H) Cuantas peliculas por genero estan o estuvieron en cartelera en el Cine Paraiso?
 aca hice un select para conseguir los ids de las peliculas de todas las funciones, luego consigo la cantidad de peliculas por genero y el genero 
 usando esos ids que consegui antes*/
-select peliculas.Genero, COUNT(peliculas.Genero) as 'Cantidad de peliculas' 
+select peliculas.Genero, COUNT(*) as 'Cantidad de peliculas' 
 from peliculas 
 where peliculas.Id in (select distinct Id_Pelicula from funciones)
 group by peliculas.Genero
@@ -360,12 +448,17 @@ select * from butacas;
 ALTER TABLE butacas DROP COLUMN precio;
 ALTER TABLE peliculas ADD precio REAL;
 
-update peliculas set precio = 100 where id=1
-update peliculas set precio = 60 where id=2
-update peliculas set precio = 600 where id=3
-update peliculas set precio = 150 where id=4
-update peliculas set precio = 5 where id=5
-/*A*/
+update peliculas set precio = 1200 where id=1
+update peliculas set precio = 650 where id=2
+update peliculas set precio = 730 where id=3
+update peliculas set precio = 850 where id=4
+update peliculas set precio = 1050 where id=5
+update peliculas set precio = 900 where id=6
+update peliculas set precio = 940 where id=7
+update peliculas set precio = 680 where id=8
+update peliculas set precio = 720 where id=9
+
+/*A) Determine el total recaudado por funcion.*/
 
 create view funciones_cantidad AS (
 	SELECT Id_Funcion as funcion, COUNT(*) as Cantidad
@@ -375,6 +468,8 @@ create view funciones_cantidad AS (
 	GROUP BY Id_Funcion
 )
 
+
+
 select DISTINCT funciones.id as funcion,
 (peliculas.precio*funciones_cantidad.cantidad) as recaudacion
 from compras 
@@ -382,8 +477,7 @@ INNER JOIN funciones on  compras.Id_Funcion = funciones.ID
 INNER JOIN peliculas on peliculas.id = funciones.Id_Pelicula 
 INNER JOIN funciones_cantidad on funciones.id = funciones_cantidad.funcion
 
-/*B
-  Determine el promedio recaudado por función para cada pelicula. 
+/*B) Determine el promedio recaudado por función para cada pelicula. 
   Es decir, si la pelicula Argentina, 1985 tuvo dos funciones, 
   y en una recaudó 1000 pesos, y en la otra recaudó 3000 pesos, 
   el promedio recaudado por función para esta pelicula es 2000 pesos.
@@ -397,22 +491,22 @@ INNER JOIN funciones on  compras.Id_Funcion = funciones.ID
 INNER JOIN peliculas on peliculas.id = funciones.Id_Pelicula 
 INNER JOIN funciones_cantidad on funciones.id = funciones_cantidad.funcion
 )
-select SUM(funciones_recaudacion.recaudacion)/COUNT(funciones_recaudacion.pelicula) as 'Promedio recaudado por función',
+
+select ROUND(SUM(funciones_recaudacion.recaudacion)/COUNT(funciones_recaudacion.pelicula),0) as 'Promedio recaudado por función',
 funciones_recaudacion.pelicula 
 from funciones_recaudacion
 group by funciones_recaudacion.pelicula
-/*
-C: 
+
+/*C) Determine el porcentaje de entradas vendidas por funcion,, y muestre pelicula, seucursal,
+hora y dia, solo para aquellas en las que se vendio menos del 50%
 */
-select  
-DISTINCT 
+
+select  DISTINCT 
 sucursales.Localidad as sucursal, 
 funciones.id as funcion,
-CONCAT((CAST(funciones_cantidad.cantidad AS float)/CAST(salas.Cant_Butacas AS float))*100,'%')
-as vendidas,
-FORMAT (funciones.Fecha,'dd/MM/yyyy') as dia, 
-funciones.Horario  as hora ,
-peliculas.Nombre as pelicula
+
+CONCAT((CAST(funciones_cantidad.cantidad AS float)/CAST(salas.Cant_Butacas AS float))*100,'%') as vendidas,
+FORMAT (funciones.Fecha,'dd/MM/yyyy') as dia, funciones.Horario  as hora ,peliculas.Nombre as pelicula
 from compras 
 INNER JOIN funciones on  compras.Id_Funcion = funciones.ID
 INNER JOIN peliculas on peliculas.id = funciones.Id_Pelicula 
@@ -420,14 +514,13 @@ INNER JOIN funciones_cantidad on funciones.id = funciones_cantidad.funcion
 INNER JOIN butacas on compras.Id_Butaca = butacas.id
 INNER JOIN salas on salas.Id = butacas.Id_Salas
 INNER JOIN sucursales on sucursales.Id = salas.Id_Sucursal 
+
 WHERE (CAST(funciones_cantidad.cantidad AS float)/CAST(salas.Cant_Butacas AS float))*100 < 50
 
-/*
-D
-Revisar
-*/
+/* D) Determine, para cada pelicula, cual fue la funcion que mas recaudó.
+*/ 
+
 select funcion,pelicula,recaudacion from funciones_recaudacion where funciones_recaudacion.recaudacion in ( 
 select MAX(funciones_recaudacion.recaudacion) as 'recaudacion'
 from funciones_recaudacion
 group by funciones_recaudacion.pelicula)
-
